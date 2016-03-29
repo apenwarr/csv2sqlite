b6d7087747b84ddd8c3556ceacc9ff3e1b17d43d
Avery Pennarun <apenwarr@gmail.com>
2016-03-29 00:01:56 -0400

    Import real wvcsv.{cc,h} from wvcsv project.

--- wvcsv.cc
+++ wvcsv.cc
@@ -1,1 +1,221 @@
-../cli/wvcsv.cc
+#include "wvcsv.h"
+#include "wvstream.h"
+#include "wvstrutils.h"
+#ifndef assert
+#include <assert.h>
+#endif
+
+
+WvString wvcsv_quote(WvStringParm s)
+{
+    if (s.isnull())
+	return ""; // unquoted blankness means "null"
+    else if (s == "")
+	return "\"\""; // quoted blankness means "empty string"
+    else if (strcspn(s, ",\"\r\n") < s.len())
+    {
+	// if it has quotes or commas, it needs to be quoted properly
+	WvString s2;
+	s2.setsize(s.len()*2 + 2 + 1);
+	char *optr = s2.edit();
+	*optr++ = '\"';
+	for (const char *iptr = s; *iptr; iptr++)
+	{
+	    if (*iptr == '\"')
+		*optr++ = *iptr;
+	    *optr++ = *iptr;
+	}
+	*optr++ = '\"';
+	*optr++ = 0;
+	return s2;
+    }
+    else
+	return s; // the string doesn't need quoting: leave it as-is
+}
+
+
+WvString wvcsv_quote(WVSTRING_FORMAT_DEFN)
+{
+    return wvcsv_quote(WvString(WVSTRING_FORMAT_CALL));
+}
+
+
+char *wvcsv_readline(WvStream &stream, WvBuf &buf)
+{
+    bool need_read = false;
+    do
+    {
+	if (need_read)
+	{
+	    stream.runonce();
+	    stream.read(buf, 16384);
+	}
+	
+	size_t len = buf.used();
+	const unsigned char *sptr = buf.peek(0, len), *eptr = sptr + len;
+	bool inquote = false;
+	for (const unsigned char *cptr = sptr; cptr < eptr; cptr++)
+	{
+	    if (*cptr == '"')
+		inquote = !inquote;
+	    else if (!inquote && *cptr == '\n')
+	    {
+		// end of a line
+		len = cptr - sptr + 1;
+		unsigned char *s = (unsigned char *)buf.get(len);
+		assert(s[len-1] == '\n');
+		s[len-1] = 0;
+		if (len > 1 && s[len-2] == '\r')
+		    s[len-2] = 0;
+		return (char *)s;
+	    }
+	}
+	
+	// finished loop without finding anything
+	need_read = true;
+    } while (stream.isok());
+    
+    // if we get here, the stream is dead, so return whatever we can
+    if (buf.used())
+    {
+	buf.put('\0');
+	return (char *)buf.get(buf.used());
+    }
+    else
+	return NULL;
+}
+
+
+char *wvcsv_dequote(const char *in, char *out, size_t *length)
+{
+    if (!*in)
+    {
+	if (length) *length = 0;
+	return NULL; // unquoted blankness is a null string
+    }
+    else if (!strcmp(in, "\"\""))
+    {
+	*out = 0;
+	if (length) *length = 0;
+	return out; // quoted blankness is an empty string
+    }
+    else
+    {
+	char *optr = out;
+	const char *cptr = in;
+
+	// Ignore one leading double quote.  If it starts with a double quote,
+	// it must have been quoted on the encoding side, so the wrapping
+	// quotes must be discarded.  The trailing quote will be automatically
+	// dropped because it won't have a pair.
+	if (in[0] == '"')
+	    cptr++;
+
+	for (; *cptr; cptr++)
+	{
+	    if (*cptr == '"')
+	    {
+		if (cptr[1] == '"')
+		{
+		    cptr++;
+		    *optr++ = '"';
+		}
+	    }
+	    else
+		*optr++ = *cptr;
+	}
+	*optr = 0;
+	if (length) *length = optr - out;
+	return out;
+    }
+}
+
+
+inline char *dequote_or_not(char *tofutz, bool dequote, size_t *length)
+{
+    return dequote ? wvcsv_dequote(tofutz, tofutz, length) : tofutz;
+}
+
+void wvcsv_splitline(std::vector<char*> &l, std::vector<size_t> &ll,
+		     char *s, size_t slen,
+		     bool dequote_values)
+{
+    bool inquote = false;
+    int istart = 0;
+    
+    l.clear();
+    ll.clear();
+    
+    for (size_t i = 0; i < slen; i++)
+    {
+	if (s[i] == '"')
+	    inquote = !inquote;
+	else if (!inquote && s[i] == ',')
+	{
+	    // end of a column
+	    s[i] = 0;
+	    size_t length = i - istart;
+	    char *p = dequote_or_not(s+istart, dequote_values, &length);
+	    l.push_back(p);
+	    ll.push_back(length);
+	    //assert(!p || strlen(p) == length);
+	    istart = i + 1;
+	}
+    }
+    
+    size_t xlength = slen - istart;
+    l.push_back(dequote_or_not(s+istart, dequote_values, &xlength));
+    ll.push_back(xlength);
+    assert(l.size() == ll.size());
+}
+
+
+void wvcsv_splitline_slow(WvStringList &l, char *s, size_t slen)
+{
+    std::vector<char*> v;
+    std::vector<size_t> lv;
+    wvcsv_splitline(v, lv, s, slen);
+    
+    for (std::vector<char*>::iterator i = v.begin(); i < v.end(); i++)
+	l.append(*i);
+}
+
+
+WvCsvIter::WvCsvIter(WvStream &_stream, bool expect_TABLE, bool expect_headers,
+		     bool dequote_values)
+    : stream(_stream), dequote(dequote_values)
+{
+    if (expect_TABLE)
+    {
+	firstline = stream.getline(-1);
+	if (!firstline || strncmp(firstline, "TABLE ", 6) != 0)
+	    err.set("First line should start with TABLE");
+    }
+    if (!firstline) firstline = "";
+    
+    if (expect_headers && err.isok())
+    {
+	// headerline must be a class member since headers[] is a bunch of
+	// pointers into it
+	headerline = wvcsv_readline(stream, buf);
+	if (headerline)
+	{
+	    std::vector<size_t> lv;
+	    wvcsv_splitline(headers, lv, headerline.edit(), headerline.len());
+	}
+	else
+	    err.set("CSV header line missing");
+    }
+}
+
+
+bool WvCsvIter::next()
+{
+    // 'line' contents are stored in buf, and will be invalidated on the next
+    // call to wvcsv_readline
+    if (!err.isok()) return false;
+    char *line = wvcsv_readline(stream, buf);
+    if (!line || !line[0] || !strcmp(line, "\r")) return false;
+    wvcsv_splitline(cols, lengths, line, strlen(line), dequote);
+    return true;
+}
--- wvcsv.h
+++ wvcsv.h
@@ -1,1 +1,118 @@
-../cli/wvcsv.h
+#ifndef __WVCSV_H
+#define __WVCSV_H
+
+#include "wvstring.h"
+#include "wvbuf.h"
+#include "wverror.h"
+#include <vector>
+
+class WvStream;
+class WvStringList;
+
+/**
+ * Quote the given string using CSV-compatible parsing rules.
+ * 
+ * You can combine a series of such quoted strings into a valid "line" of CSV
+ * by simply concatenating them with commas in between.
+ * 
+ * We only add actual quotation marks if leaving them out would make parsing
+ * ambiguous.
+ * 
+ * It *is* valid for s to contain newline characters.  This will produce an
+ * output CSV line that contains newlines, but parsing is not ambiguous because
+ * the newlines will be inside quotes.
+ * 
+ * Note: for NULL pointers we produce an empty string ("").  For empty strings,
+ * we produce the empty quoted string ("\"\"").  This arbitrary rule can be
+ * used to differentiate between the two cases upon decoding, but a decoder
+ * that doesn't know this rule can simply treat them both as empty strings.
+ */
+WvString wvcsv_quote(WvStringParm s);
+
+// convenience function
+WvString wvcsv_quote(WVSTRING_FORMAT_DECL);
+
+/**
+ * Reverse the quoting operation from wvcsv_quote().
+ * 
+ * Note: it is explicitly okay for 'out' to point at the same buffer as 'in',
+ * since dequoting a string never increases its length.
+ * 
+ * Returns 'out', if the dequoted string is non-null, otherwise NULL.
+ * 
+ * WARNING: you probably shouldn't use this function directly.  The naive
+ * implementation of just reading lines of text, splitting on commas, and
+ * dequoting the columns is broken for lots of reasons.
+ * 
+ * Use wvcsv_readline() and wvcsv_splitline() instead.
+ */
+char *wvcsv_dequote(const char *in, char *out, size_t *length = NULL);
+
+/**
+ * Given one "line" of a CSV file, split and decode the columns into l.
+ * 
+ * WARNING: this function modifies 's' in order to go faster.  If you don't like
+ * this, make a copy first.
+ * 
+ * WARNING: a "line" of a CSV file may contain newlines.  You should read the
+ * file using wvcsv_readline() if you don't want to screw up.
+ */
+void wvcsv_splitline(std::vector<char*> &l, std::vector<size_t> &ll,
+		     char *s, size_t slen,
+		     bool dequote_values = true);
+
+/**
+ * Just like wvcsv_splitline, but filling a WvStringList.
+ * 
+ * This is slower than using a vector of char*, because it needs to make a copy
+ * of all the strings.  But it's useful if you want to call things like join().
+ */
+void wvcsv_splitline_slow(WvStringList &l, char *s, size_t slen);
+
+/**
+ * Given a stream and a temporary buffer, return the next full CSV line.
+ * 
+ * This function reads the next valid CSV line from stream and returns that line.
+ * The returned line is suitable for splitting with wvcsv_splitline().  Returns
+ * NULL if the stream can't be read for any reason.  (If you want to know the
+ * reason, check stream's error status.)
+ * 
+ * You need to pass the same value for 'buf' every time you call this function
+ * on a given stream.  Upon return, buf may contain part of the next CSV line(s),
+ * which are important in order to parse the next line.
+ * 
+ * WARNING: the returned buffer is a pointer directly into 'buf'.  Doing
+ * any further operations on 'buf' (including get()) will make the returned
+ * pointer invalid.  If this bothers you, copy it immediately into a WvString.
+ * 
+ * WARNING: after calling wvcsv_readline() on an input stream, the stream is
+ * not really useful for reading anything else, because of the way we store
+ * extra bytes in buf.
+ */
+char *wvcsv_readline(WvStream &stream, WvBuf &buf);
+
+
+class WvCsvIter
+{
+    WvStream &stream;
+    WvDynBuf buf;
+    WvString headerline;
+    bool dequote;
+    mutable std::vector<char*> cols;
+public:
+    mutable std::vector<size_t> lengths;
+    WvString firstline;
+    WvError err;
+    std::vector<char*> headers;
+    
+    WvCsvIter(WvStream &_stream, bool expect_TABLE, bool expect_headers,
+	      bool dequote_values = true);
+    bool next();
+    
+    std::vector<char*> *ptr() const
+        { return &cols; }
+    WvIterStuff(std::vector<char*>);
+};
+
+
+#endif // __WVCSV_H
